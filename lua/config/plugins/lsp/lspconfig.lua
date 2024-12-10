d4ad04bc4aa16641ea5cfe18b00d07f620aab64c
htun <yuren1248@qq.com>
2024-12-10 11:01:35 +0800

    Disable lsp corresponding support

--- lua/config/plugins/lsp/lspconfig.lua
+++ lua/config/plugins/lsp/lspconfig.lua
@@ -1,6 +1,6 @@
 return {
+  --[[
   "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
@@ -91,4 +91,5 @@ return {
       end,
     })
   end,
+  --]]
 }
