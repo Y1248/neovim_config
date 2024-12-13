860d75431a665312d1d3a4ac9cbc084aee83b043
htun <yuren1248@qq.com>
2024-12-13 14:25:02 +0800

    Add some keymap: 'jf' to <ESC>, buffer management, etc

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -2,9 +2,13 @@ vim.g.mapleader = ','
 local keymap = vim.keymap
 
 keymap.set('n', '<leader>w', '<cmd>w<CR>', {desc = 'Save file'})
+keymap.set('i', 'jf', '<ESC>', { silent = true })
+keymap.set('v', 'jf', '<ESC>', { silent = true })
 
 -- Basic buffer management
+keymap.set('n', '<leader>bb', ':buffers<cr>:b<space>', { silent = true })
 keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', {desc = 'Close current buffer'})
+keymap.set('n', '<leader>q', '<cmd>bdelete<CR>', {desc = 'Close current buffer'})
 -- Addtional buffer management: check bufferline.lua file
 
 -- Window Management
@@ -12,10 +16,11 @@ keymap.set('n', '<leader>sv', '<C-w>v', {desc = 'Split window vertically'})
 keymap.set('n', '<leader>sh', '<C-w>s', {desc = 'Split window horizontally'})
 keymap.set('n', '<leader>se', '<C-w>=', {desc = 'Make splits equal size'})
 keymap.set('n', '<leader>sx', '<cmd>close<CR>', {desc = 'Close current window'})
-keymap.set('n', '<C-h>', '<C-w>h', { silent = true})
-keymap.set('n', '<C-j>', '<C-w>j', { silent = true})
-keymap.set('n', '<C-k>', '<C-w>k', { silent = true})
-keymap.set('n', '<C-l>', '<C-w>l', { silent = true})
+keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
+keymap.set('n', '<C-o>', '<C-w>o', { silent = true })
 
 -- Tab management
 keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', {desc = 'Open new tab'})
