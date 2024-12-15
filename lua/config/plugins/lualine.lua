d340fb4c924880bc4f55695fc45a80c477db33f6
htun <yuren1248@qq.com>
2024-12-15 14:48:33 +0800

    lualine: change style of diagnostic part

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -30,12 +30,34 @@ return {
           {
             'diff',
             -- fmt = function(res) return res .. ' ' end,
-            -- padding = 0
+            padding = 0
           },
           {
             'diagnostics',
+
+            -- Table of diagnostic sources, available sources are:
+            --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+            -- or a function that returns a table as such:
+            --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+            sources = { 'nvim_diagnostic', 'coc' },
+
+            -- Displays diagnostics for the defined severity types
+            sections = { 'error', 'warn', 'info', 'hint' },
+
+            diagnostics_color = {
+              -- Same values as the general color option can be used here.
+              error = 'DiagnosticError', -- Changes diagnostics' error color.
+              warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+              info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+              hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+            },
+            symbols = {error = '󰯸 ', warn = '󰰮 ', info = '󰰄 ', hint = '󰰁 '},
+            -- symbols = {error = ' ', warn = ' ', info = '󰠠 ', hint = ' '},
+            colored = true,           -- Displays diagnostics status in color if set to true.
+            update_in_insert = false, -- Update diagnostics in insert mode.
+            always_visible = false,   -- Show diagnostics even if there are none.
             -- fmt = function(res) return res .. ' ' end,
-            -- padding = 0
+            padding = 0
           }
         },
         lualine_c = { 'filename' },
