0c24c225372fb64b457974055d6c3d6bbfd99944
htun <yuren1248@qq.com>
2024-12-15 13:26:13 +0800

    Enable typst about configuration

--- lua/config/plugins/typst.lua
+++ lua/config/plugins/typst.lua
@@ -1,16 +1,23 @@
 return {
-  --[[
+  ---[[
   { -- Quick preview for typst files
     'chomosuke/typst-preview.nvim',
     ft = 'typst',
     -- event = { 'VeryLazy', 'BufReadPre', 'BufNewFile' },
     version = '1.*',
     opts = {},
+    config = function()
+      require('typst-preview').setup({
+        dependencies_bin = {
+          ['tinymist'] = 'tinymist-win32-x64.exe',
+        }
+      })
+    end
   },
+  --]]
   { -- typst support for neovim
     'kaarmu/typst.vim',
     ft = 'typst',
     -- event = { 'VeryLazy', 'BufReadPre', 'BufNewFile' },
   },
-  --]]
 }
