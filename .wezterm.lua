c2905fe6f1cd6c99fa6a46608775bc2a92558966
htun <yuren1248@qq.com>
2024-12-15 23:39:59 +0800

    Update configuration of WezTerm

---
+++ .wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require('wezterm')
+
+local config = wezterm.config_builder()
+
+-- Appearance
+config.color_scheme = 'Mathias'
+config.hide_tab_bar_if_only_one_tab = false
+config.show_new_tab_button_in_tab_bar = true
+config.window_decorations = 'RESIZE'
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.initial_rows = 28
+config.initial_cols = 85
+config.enable_scroll_bar = false
+config.window_frame = {
+  font = wezterm.font{family = 'Courier New', weight = 'Regular'},
+}
+
+-- Set default program
+config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+
+-- Font
+config.line_height = 0.8
+config.font = wezterm.font('SauceCodePro Nerd Font', { weight = 'Regular' })
+-- config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Regular' })
+-- config.font = wezterm.font('JetBrainsMonoNL Nerd Font')
+
+return config
