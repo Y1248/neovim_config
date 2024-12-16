b9fa4fc309c7e8a328a59ba14a322dd77a14102b
htun <yuren1248@qq.com>
2024-12-16 10:58:11 +0800

    Change some comment for oh-my-posh config

--- other_configs/Microsoft.PowerShell_profile.ps1
+++ other_configs/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 # C:\Users\renyu\Documents\PowerShell\
 # New-Item -Path $PROFILE -Type File -Force
-# oh-my-posh init
+# Copy content of this file to $PROFILE (which is powershell's configuration file)
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/wopian.omp.json" | Invoke-Expression
 
 Set-Alias -Name v -Value nvim
