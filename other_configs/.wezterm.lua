aa2377c2462531bc8b894e88b7903d60c4c39d4c
htun <yuren1248@qq.com>
2024-12-17 16:45:55 +0800

    WezTerm: update theme

--- other_configs/.wezterm.lua
+++ other_configs/.wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 -- Appearance
-config.color_scheme = 'Mathias'
+config.color_scheme = 'Thayer Bright'
 config.hide_tab_bar_if_only_one_tab = false
 config.show_new_tab_button_in_tab_bar = true
 config.window_decorations = 'RESIZE'
