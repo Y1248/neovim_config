518aab62fdcbc1397a205145f78e120992bb97fc
htun <yuren1248@qq.com>
2024-12-20 22:39:37 +0800

    vscode colorscheme: make identifier of function and keyword to be bold

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -5,7 +5,19 @@ return {
     require('vscode').setup({
       style = 'dark',
       group_overrides = {
-        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightBlue, bold=true },
+        Cursor = { fg = c.vscDarkBlue, bg = c.vscLightBlue, bold = true },
+
+        ["@function"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+        ["@function.builtin"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+        ["@function.macro"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+        ["@function.method"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+
+        ["@keyword"] = { fg = c.vscBlue, bg = 'NONE', bold = true },
+        ["@keyword.conditional"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        ["@keyword.repeat"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        ["@keyword.return"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        ["@keyword.exception"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        ["@keyword.import"] = { fg = c.vscPink, bg = 'NONE', bold = true },
       }
     })
     vim.cmd('colorscheme vscode')
