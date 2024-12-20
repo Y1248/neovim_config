854bad6299d5ecf2cf364fd2094cf016c14cea09
htun <yuren1248@qq.com>
2024-12-20 15:03:45 +0800

    Set the color of cursor

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,6 +1,13 @@
 return {
   'Mofiqul/vscode.nvim',
   config = function()
+    local c = require('vscode.colors').get_colors()
+    require('vscode').setup({
+      style = 'dark',
+      group_overrides = {
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightBlue, bold=true },
+      }
+    })
     vim.cmd('colorscheme vscode')
   end
 }
