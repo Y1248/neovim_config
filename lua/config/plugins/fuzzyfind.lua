d125842858f42fc5f02d512fc6fca001b8326cba
htun <yuren1248@qq.com>
2025-01-10 11:21:44 +0800

    Recover from fzf-lua to mini.pick since I want to use a light weight one

--- lua/config/plugins/fuzzyfind.lua
+++ lua/config/plugins/fuzzyfind.lua
@@ -1,12 +1,37 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { '<leader>ff', '<CMD>FzfLua files<CR>', { slient = true } },
-    { '<leader>fb', '<CMD>FzfLua buffers<CR>', { slient = true } },
-    { '<leader>fs', '<CMD>FzfLua grep<CR>', { silent = true } },
-    { '<leader>fc', '<CMD>FzfLua grep_cword<CR>', { silent = true } },
-    { '<leader>fr', '<CMD>FzfLua resume<CR>', { silent = true } },
+  {
+    'echasnovski/mini.pick',
+    cmd = { 'Pick' },
+    dependencies = {
+      'echasnovski/mini.extra',
+    },
+    keys = {
+      { '<leader>ff', '<cmd>Pick files<cr>',  desc = 'Fuzzy find files' },
+      { '<leader>fr', '<cmd>Pick resume<cr>',  desc = 'Resume last search result' },
+      { '<leader>fs', '<cmd>Pick grep_live<cr>',  desc = 'Find string' },
+    },
+    config = function()
+      require('mini.pick').setup({
+        window = {
+          prompt_cursor = '█',
+          prompt_prefix = '',
+        }
+      })
+      require('mini.extra').setup({})
+    end
   },
-  opts = {},
+  --[[
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { '<leader>ff', '<CMD>FzfLua files<CR>', { slient = true } },
+      { '<leader>fb', '<CMD>FzfLua buffers<CR>', { slient = true } },
+      { '<leader>fs', '<CMD>FzfLua grep<CR>', { silent = true } },
+      { '<leader>fc', '<CMD>FzfLua grep_cword<CR>', { silent = true } },
+      { '<leader>fr', '<CMD>FzfLua resume<CR>', { silent = true } },
+    },
+    opts = {},
+  }
+  --]]
 }
--- lua/config/plugins/lsp/lspconfig.lua
+++ lua/config/plugins/lsp/lspconfig.lua
@@ -3,28 +3,29 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
     { 'hrsh7th/cmp-nvim-lsp' },
-    { 'ibhagwan/fzf-lua' },
+    { 'echasnovski/mini.pick'},
+    { 'echasnovski/mini.extra'},
     { 'williamboman/mason.nvim', }
   },
   config = function()
     local lspconfig = require('lspconfig')
     local mason_lspconfig = require('mason-lspconfig')
     local cmp_nvim_lsp = require('cmp_nvim_lsp')
+    local mini_extra = require('mini.extra')
 
     local keymap = vim.keymap -- for conciseness
     local opts = { silent = true }
 
     -- set keybinds
-    keymap.set('n', 'gR', '<CMD>FzfLua lsp_references<CR>', opts) -- show definition, references
-    keymap.set('n', 'gD', '<CMD>FzfLua lsp_declarations<CR>', opts) -- go to declaration
-    keymap.set('n', 'gd', '<CMD>FzfLua lsp_definitions<CR>', opts) -- show lsp definitions
-    keymap.set('n', 'gi', '<CMD>FzfLua lsp_implementations<CR>', opts) -- show lsp implementations
-    keymap.set('n', 'gt', '<CMD>FzfLua lsp_typedefs<CR>', opts) -- show lsp type definitions
-
+    keymap.set('n', 'gR', function() mini_extra.pickers.lsp({ scope = 'references' }) end, opts) -- show definition, references
+    keymap.set('n', 'gD', function() mini_extra.pickers.lsp({ scope = 'declaration' }) end, opts) -- go to declaration
+    keymap.set('n', 'gd', function() mini_extra.pickers.lsp({ scope = 'definition' }) end, opts) -- show lsp definitions
+    keymap.set('n', 'gi', function() mini_extra.pickers.lsp({ scope = 'implementation' }) end, opts) -- show lsp implementations
+    keymap.set('n', 'gt', function() mini_extra.pickers.lsp({ scope = 'type_definition' }) end, opts) -- show lsp type definitions
     keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
     keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- smart rename
 
-    keymap.set('n', '<leader>D', '<CMD>FzfLua diagnostics_document<CR>', opts) -- show  diagnostics for file
+    keymap.set('n', '<leader>D', function() mini_extra.gen_ai_spec.diagnostic() end, opts) -- show  diagnostics for file
     keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
     keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
     keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
