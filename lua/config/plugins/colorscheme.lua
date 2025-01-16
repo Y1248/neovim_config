185c4d3e99ee09b3d0e2eda945082ee3b73263e6
htun <yuren1248@qq.com>
2025-01-16 11:15:02 +0800

    Update colorscheme to dark background

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -27,7 +27,7 @@ return {
         }
       })
       -- vim.opt.background = 'light'
-      vim.cmd('colorscheme catppuccin-latte')
+      vim.cmd('colorscheme catppuccin-mocha')
     end
   },
   --]]
--- other_configs/.wezterm.lua
+++ other_configs/.wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 -- Appearance
--- config.color_scheme = 'Dark Pastel'
-config.color_scheme = 'Catppuccin Latte'
+config.color_scheme = 'Dark Pastel'
+-- config.color_scheme = 'Catppuccin Latte'
 config.hide_tab_bar_if_only_one_tab = false
 config.show_new_tab_button_in_tab_bar = true
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
