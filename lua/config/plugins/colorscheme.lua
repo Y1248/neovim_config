1d5283d37efb5aef3734ba7df40097ba54437988
htun <yuren1248@qq.com>
2025-01-19 11:09:20 +0800

    Recover colorscheme from catppuccin to vscode

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,14 +1,33 @@
 return {
-  --[[
+  ---[[
   {
     'Mofiqul/vscode.nvim',
     config = function()
-      require('vscode').setup()
+      local c = require('vscode.colors').get_colors()
+      require('vscode').setup({
+        style = 'dark',
+        group_overrides = {
+          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightBlue, bold = true },
+          ["@function"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+          ["@function.builtin"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+          ["@function.macro"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+          ["@function.method"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+          ["@keyword"] = { fg = c.vscBlue, bg = 'NONE', bold = true },
+          ["@keyword.conditional"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+          ["@keyword.repeat"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+          ["@keyword.return"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+          ["@keyword.exception"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+          ["@keyword.import"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+          ['@number'] =  { fg = c.vscLightGreen, bg = 'NONE', bold = true },
+          ['@number.float'] = { fg = c.vscLightGreen, bg = 'NONE', bold = true },
+          ['@boolean'] = { fg = c.vscBlue, bg = 'NONE', bold = true },
+        }
+      })
       vim.cmd('colorscheme vscode')
     end
   },
   --]]
-  ---[[
+  --[[
   {
     'catppuccin/nvim',
     config = function()
