c1e71a365f7e3ef3dbdd09cfc35b04ce0941da4e
htun <yuren1248@qq.com>
2025-02-06 22:44:16 +0800

    Update wezterm configuration

--- other_configs/.wezterm.lua
+++ other_configs/.wezterm.lua
@@ -3,18 +3,22 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 -- Appearance
-config.color_scheme = 'Dark Pastel'
+config.color_scheme = 'VSCodeDark+ (Gogh)'
+-- config.color_scheme = 'Dark Pastel'
 -- config.color_scheme = 'Catppuccin Latte'
 config.hide_tab_bar_if_only_one_tab = false
 config.show_new_tab_button_in_tab_bar = true
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 config.integrated_title_button_style = 'Windows'
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.initial_rows = 36
-config.initial_cols = 110
+config.initial_rows = 28
+config.initial_cols = 90
 config.enable_scroll_bar = false
 config.window_frame = {
-  font = wezterm.font{family = 'Fira Code', weight = 'Regular'},
+  font = wezterm.font{family = 'Courier New', weight = 'Regular'},
+}
+config.colors = {
+  cursor_bg = '#9CDCFE',
 }
 
 -- Set default program
@@ -22,6 +26,7 @@ config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
 
 -- Font
 config.line_height = 0.9
+config.font_size = 12
 config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Regular' })
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0'} -- Disable font ligatures
 
@@ -35,3 +40,4 @@ config.keys = {
 }
 
 return config
+
