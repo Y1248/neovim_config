a6bb87e84b847c4ea484f5e1587ee1bfe271a650
htun <yuren1248@qq.com>
2025-02-07 15:54:41 +0800

    Change colorscheme to light version

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -5,23 +5,23 @@ return {
     config = function()
       local c = require('vscode.colors').get_colors()
       require('vscode').setup({
-        style = 'dark',
-        group_overrides = {
-          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightBlue, bold = true },
-          ["@function"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
-          ["@function.builtin"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
-          ["@function.macro"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
-          ["@function.method"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
-          ["@keyword"] = { fg = c.vscBlue, bg = 'NONE', bold = true },
-          ["@keyword.conditional"] = { fg = c.vscPink, bg = 'NONE', bold = true },
-          ["@keyword.repeat"] = { fg = c.vscPink, bg = 'NONE', bold = true },
-          ["@keyword.return"] = { fg = c.vscPink, bg = 'NONE', bold = true },
-          ["@keyword.exception"] = { fg = c.vscPink, bg = 'NONE', bold = true },
-          ["@keyword.import"] = { fg = c.vscPink, bg = 'NONE', bold = true },
-          ['@number'] =  { fg = c.vscLightGreen, bg = 'NONE', bold = true },
-          ['@number.float'] = { fg = c.vscLightGreen, bg = 'NONE', bold = true },
-          ['@boolean'] = { fg = c.vscBlue, bg = 'NONE', bold = true },
-        }
+        style = 'light',
+        -- group_overrides = {
+        --   Cursor = { fg = c.vscDarkBlue, bg = c.vscLightBlue, bold = true },
+        --   ["@function"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+        --   ["@function.builtin"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+        --   ["@function.macro"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+        --   ["@function.method"] = { fg = c.vscYellow, bg = 'NONE', bold = true },
+        --   ["@keyword"] = { fg = c.vscBlue, bg = 'NONE', bold = true },
+        --   ["@keyword.conditional"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        --   ["@keyword.repeat"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        --   ["@keyword.return"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        --   ["@keyword.exception"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        --   ["@keyword.import"] = { fg = c.vscPink, bg = 'NONE', bold = true },
+        --   ['@number'] =  { fg = c.vscLightGreen, bg = 'NONE', bold = true },
+        --   ['@number.float'] = { fg = c.vscLightGreen, bg = 'NONE', bold = true },
+        --   ['@boolean'] = { fg = c.vscBlue, bg = 'NONE', bold = true },
+        -- }
       })
       vim.cmd('colorscheme vscode')
     end
