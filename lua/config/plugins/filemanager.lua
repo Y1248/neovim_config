72b8b61a83841ad5f7c395be642734cbc3aefeef
htun <yuren1248@qq.com>
2025-02-09 15:24:55 +0800

    Using mini.files plugin

--- lua/config/plugins/filemanager.lua
+++ lua/config/plugins/filemanager.lua
@@ -1,29 +1,13 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  keys = {
-    { '<leader>ee', '<CMD>Neotree toggle=true<CR>', { silent = true } },
-    { '<leader>ef', '<CMD>Neotree reveal=true<CR>', { silent = true } },
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require('neo-tree').setup({
-      default_component_configs = {
-        indent = {
-          padding = 0,
-          indent_size = 2,
-          with_expanders = true,
-          expander_collapsed = '',
-          expander_expanded = '',
-        },
-      },
-      window = {
-        width = 30,
-      },
-    })
-  end
+  {
+    'echasnovski/mini.files',
+    keys = {
+      { '<leader>ee', function() require('mini.files').open() end, { silent = true }},
+      { '<leader>ef', function() require('mini.files').open(vim.api.nvim_buf_get_name(0)) end, { silent = true }},
+      { '<leader>er', function() require('mini.files').open(vim.api.nvim_buf_get_name(0), false) end, { silent = true }},
+    },
+    config = function()
+      require('mini.files').setup({})
+    end
+  }
 }
