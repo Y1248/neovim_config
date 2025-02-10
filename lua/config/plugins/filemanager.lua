e3e9da2a7ad293046ded1d6037555551ec82215b
htun <yuren1248@qq.com>
2025-02-10 16:29:03 +0800

    Change filemanager to nvim-tree

--- lua/config/plugins/filemanager.lua
+++ lua/config/plugins/filemanager.lua
@@ -1,4 +1,5 @@
 return {
+  --[[
   {
     'echasnovski/mini.files',
     keys = {
@@ -8,6 +9,50 @@ return {
     },
     config = function()
       require('mini.files').setup({})
+    end
+  },
+  --]]
+  {
+    'nvim-tree/nvim-tree.lua',
+    lazy = true,
+    keys = {
+      { '<leader>ee', '<cmd>NvimTreeToggle<CR>', desc = 'NvimTreeToggle'},
+      { '<leader>ec', '<cmd>NvimTreeCollapse<CR>', desc = 'NvimTreeCollapse'},
+      { '<leader>er', '<cmd>NvimTreeRefresh<CR>', desc = 'NvimTreeRefresh'},
+      { '<leader>ef', '<cmd>NvimTreeFindFile<CR>', desc = 'NvimTreeFindFile'},
+    },
+    config = function()
+      local nvimtree = require('nvim-tree')
+      -- recommended settings from nvim-tree documentation
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      nvimtree.setup({
+        view = { width = 25, },
+        -- change folder arrow icons
+        renderer = {
+          indent_markers = { enable = true, },
+          icons = {
+            glyphs = {
+              folder = {
+                arrow_closed = '', -- arrow when folder is closed
+                arrow_open = '', -- arrow when folder is open
+              },
+            },
+          },
+        },
+        -- disable window_picker for explorer to work well with window splits
+        actions = {
+          open_file = {
+            window_picker = {
+              enable = false,
+            },
+          },
+        },
+        filters = {
+          custom = { '^\\.git', },
+        },
+        git = { ignore = false, },
+      })
     end
   }
 }
