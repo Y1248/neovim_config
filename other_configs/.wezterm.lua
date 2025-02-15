c22eb1bb75c7816303307d0cb1d4e8bc01038c4b
htun <yuren1248@qq.com>
2025-02-15 23:12:42 +0800

    Update wezterm config

--- other_configs/.wezterm.lua
+++ other_configs/.wezterm.lua
@@ -3,31 +3,32 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 -- Appearance
-config.color_scheme = 'Vs Code Light+ (Gogh)'
--- config.color_scheme = 'Dark Pastel'
--- config.color_scheme = 'Catppuccin Latte'
+config.color_scheme = 'Catppuccin Latte' --'Vs Code Light+ (Gogh)', 'Dark Pastel'
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
 config.show_new_tab_button_in_tab_bar = true
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
-config.integrated_title_button_style = 'Windows'
-config.window_padding = { left = 0, right = 0, top = 5, bottom = 0 }
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.initial_rows = 28
 config.initial_cols = 90
 config.enable_scroll_bar = false
 config.window_frame = {
-  font = wezterm.font{family = 'Courier New', weight = 'Regular'},
+  font = wezterm.font{family = 'FiraCode Nerd Font', weight = 'Regular'},
 }
--- config.colors = {
---   cursor_bg = '#9CDCFE',
--- }
+config.colors = {
+  background = '#FFFFFF',
+  cursor_bg = '#1E66F5',
+}
 
 -- Set default program
-config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+config.default_prog = { 'nu.exe' }
+-- config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
 
 -- Font
-config.line_height = 1.0
+config.line_height = 0.85
 config.font_size = 12
-config.font = wezterm.font('Lucida Console', { weight = 'Regular' })
+config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Regular' })
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0'} -- Disable font ligatures
 
 -- Keys
@@ -40,4 +41,3 @@ config.keys = {
 }
 
 return config
-
