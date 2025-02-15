3caf20f98ecad08b782d223064d22f7c32e16170
htun <yuren1248@qq.com>
2025-02-15 21:41:14 +0800

    toggleterm(update keymaps, change default shell to nushell)

--- lua/config/plugins/toggleterm.lua
+++ lua/config/plugins/toggleterm.lua
@@ -1,13 +1,17 @@
 return {
   'akinsho/toggleterm.nvim',
   keys = {
-    { '<leader>tt', '<cmd>ToggleTerm<cr>', { silent = true } },
-    { '<C-\\>', '<cmd>ToggleTerm<cr>', { silent = true } },
-    { '<leader>tl', '<cmd>ToggleTermSendCurrentLine<cr>', { silent = true } },
+    { '<leader>tt', '<CMD>ToggleTerm<CR>', { silent = true } },
+    { '<C-\\>', '<CMD>ToggleTerm<CR>', { silent = true } },
+    { '<leader>t2', "<CMD>lua require('toggleterm').exec('', 2)<CR>", { silent = true } },
+    { '<leader>t3', "<CMD>lua require('toggleterm').exec('', 3)<CR>", { silent = true } },
+    { '<leader>t4', "<CMD>lua require('toggleterm').exec('', 4)<CR>", { silent = true } },
+    { '<leader>t5', "<CMD>lua require('toggleterm').exec('', 5)<CR>", { silent = true } },
   },
   config = function()
     require('toggleterm').setup({
-      shell = 'pwsh.exe'
+      open_mapping = [[<c-\>]],
+      shell = 'nu.exe',
     })
     vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { silent = true })
   end
