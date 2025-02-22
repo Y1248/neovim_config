8f5a284e34f5cd12e853c95390212b7f0960a966
htun <yuren1248@qq.com>
2025-02-22 23:17:20 +0800

    Move definition of lsp finding about keymaps to autocompletion.lua

--- lua/config/plugins/fuzzyfind.lua
+++ lua/config/plugins/fuzzyfind.lua
@@ -1,12 +1,20 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'neovim/nvim-lspconfig',
+  },
   keys = {
     { '<C-p>', '<CMD>Telescope find_files<CR>', desc = 'Find files' },
     { '<leader>ff', '<CMD>Telescope find_files<CR>', desc = 'Find files' },
     { '<leader>fs', '<CMD>Telescope live_grep<CR>', desc = 'Find strings' },
     { '<leader>fb', '<CMD>Telescope buffers<CR>', desc = 'Find buffers' },
     { '<leader>fr', '<CMD>Telescope resume<CR>', desc = 'Find resume' },
+    { 'gR', '<CMD>Telescope lsp_references<CR>', desc = 'Lsp references' },
+    { 'gd', '<CMD>Telescope lsp_definitions<CR>', desc = 'Lsp definitions' },
+    { 'gk', '<CMD>Telescope lsp_implementations<CR>', desc = 'LSP implementations' },
+    { 'gt', '<CMD>Telescope lsp_type_definitions<CR>', desc = 'LSP type definitions' },
+    { '<leader>D', '<CMD>Telescope diagnostics<CR>', desc = 'LSP diagnostics' },
   },
   config = function()
     require('telescope').setup({})
--- lua/config/plugins/lsp/lspconfig.lua
+++ lua/config/plugins/lsp/lspconfig.lua
@@ -3,7 +3,6 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
     { 'saghen/cmp-nvim-lsp' },
-    { 'nvim-telescope/telescope.nvim' },
     { 'williamboman/mason.nvim', }
   },
   config = function()
@@ -14,14 +13,9 @@ return {
     local opts = { silent = true }
 
     -- set keybinds
-    keymap.set('n', 'gR', '<CMD>Telescope lsp_references<CR>', opts) -- show definition, references
-    keymap.set('n', 'gd', '<CMD>Telescope lsp_definitions<CR>', opts) -- show lsp definitions
-    keymap.set('n', 'gi', '<CMD>Telescope lsp_implementations<CR>', opts) -- show lsp implementations
-    keymap.set('n', 'gt', '<CMD>Telescope lsp_type_definitions<CR>', opts) -- show lsp type definitions
     keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
     keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- smart rename
 
-    keymap.set('n', '<leader>D', '<CMD>Telescope diagnostics<CR>', opts) -- show  diagnostics for file
     keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
     keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
     keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
@@ -46,13 +40,13 @@ return {
     --   }
     -- })
 
-    mason_lspconfig.setup_handlers({
-      -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = lsp_capabilities,
-        })
-      end,
-    })
+    -- mason_lspconfig.setup_handlers({
+    --   -- default handler for installed servers
+    --   function(server_name)
+    --     lspconfig[server_name].setup({
+    --       capabilities = lsp_capabilities,
+    --     })
+    --   end,
+    -- })
   end,
 }
