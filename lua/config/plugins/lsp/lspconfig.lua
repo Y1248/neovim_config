a714424b5be336d3f59f3cfb07e73c1003ca798a
htun1248 <yuren1248@qq.com>
2025-05-21 16:48:32 +0800

    Keymap(open float window for current diagnostic info): switch from `,d` to `,dd`

--- lua/config/plugins/lsp/lspconfig.lua
+++ lua/config/plugins/lsp/lspconfig.lua
@@ -17,7 +17,7 @@ return {
     keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
     keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- smart rename
 
-    keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts) -- show diagnostics for line
+    keymap.set('n', '<leader>dd', vim.diagnostic.open_float, opts) -- show diagnostics for line
     keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
     keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
     keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
