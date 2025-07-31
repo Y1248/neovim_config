b365483701d5cfff3ed61f72b6634c8249169bc5
Yu <yuren1248@qq.com>
2025-07-31 20:58:59 +0800

    Change layout for Snacks.picker

--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -24,22 +24,22 @@ return {
         preset = "ivy",
         position = "bottom",
         preview = false,
-        layout = {
-          box = "vertical",
-          backdrop = false,
-          row = -1,
-          width = 0,
-          height = 0.35,
-          border = "none",
-          title = " {title} {live} {flags}",
-          title_pos = "left",
-          { win = "input", height = 1, border = "none" },
-          {
-            box = "horizontal",
-            { win = "list", border = "none" },
-            { win = "preview", title = "{preview}", width = 0.6, border = "none" },
-          },
-        },
+        -- layout = {
+        --   box = "vertical",
+        --   backdrop = false,
+        --   row = -1,
+        --   width = 0,
+        --   height = 0.35,
+        --   border = "none",
+        --   title = " {title} {live} {flags}",
+        --   title_pos = "left",
+        --   { win = "input", height = 1, border = "none" },
+        --   {
+        --     box = "horizontal",
+        --     { win = "list", border = "none" },
+        --     { win = "preview", title = "{preview}", width = 0.6, border = "none" },
+        --   },
+        -- },
       }
     }
   },
